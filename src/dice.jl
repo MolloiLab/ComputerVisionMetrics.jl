1a17d466b92d902c60dc14c0e5f98c66aa63e770
Dale Black <djblack@uci.edu>
2023-10-04 16:26:32 -0700

    update

--- src/dice.jl
+++ src/dice.jl
@@ -1,5 +1,20 @@
-function dice_metric(seg_pred, seg_gt)
-	return 2 * sum((seg_pred .& seg_gt)) / (sum(seg_pred) + sum(seg_gt))
+
+"""
+	dice_metric(prediction::AbstractArray, ground_truth::AbstractArray)
+
+Compute the Dice Coefficient (also known as the Sørensen–Dice index) between two binary masks, `prediction` and `ground_truth`.
+
+The Dice Coefficient measures the similarity between two sets and ranges from 0 (no overlap) to 1 (perfect overlap).
+
+#### Arguments
+- `predicted_segment`: Predicted binary mask.
+- `ground_truth_segment`: Ground truth binary mask.
+
+#### Returns
+- Dice Coefficient value between the two masks.
+"""
+function dice_metric(prediction::AbstractArray, ground_truth::AbstractArray)
+    return 2 * sum((prediction .& ground_truth)) / (sum(prediction) + sum(ground_truth))
 end
 
 export dice_metric
